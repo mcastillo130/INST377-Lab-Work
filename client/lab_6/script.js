74a431442b78160dc72ba5bfde9cefadbaac3b77
Mario Castillo <mcastillo130@gmail.com>
2022-04-10 21:22:20 -0400

    Update script.js

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -13,18 +13,20 @@ for (let i=0; i < 1000; i++) {
 }
 */
 
-
-
-function dataHandler(dataArray) {
+function restoArrayMake(dataArray) {
   console.log('fired dataHandler');
-  console.table(dataArray); // this is called "dot notation"
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
     const restNum = getRandomIntInclusive(0, dataArray.length - 1);
     return dataArray[restNum];
   });
+  // console.log(listItems);
+  return listItems;
+}
 
-
+function createHtmlList(collection) {
+  console.log(collection);
+}
 
 // As the last step of your lab, hook this up to index.html
 async function mainEvent() { // the async keyword means we can make API request
@@ -36,7 +38,7 @@ async function mainEvent() { // the async keyword means we can make API request
   const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
   console.log(arrayFromJson);
-  
+
   if (arrayFromJson.data.length > 0) {
     submit.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
@@ -44,7 +46,8 @@ async function mainEvent() { // the async keyword means we can make API request
       console.log('form submission'); // this is substituting for a "breakpoint"
       // arrayFromJson.data - we're accessing a key called 'data' on the returned object
       // it contains all 1,000 records we need
-      dataHandler(arrayFromJson.data);
+      const restoArray = restoArrayMake(arrayFromJson.data);
+      createHtmlList(restoArray);
     });
   }
 }
